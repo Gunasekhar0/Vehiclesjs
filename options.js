5efd3210e59fdc7445b383e3d599a4177b80e315
Gunasekhar0 <127474265+Gunasekhar0@users.noreply.github.com>
2023-07-19 10:39:35 +0530

    Add files via upload

---
+++ options.js
@@ -0,0 +1,9 @@
+function car(){
+    document.getElementById("car").src="https://freepngimg.com/thumb/car/3-2-car-free-download-png.png"
+}
+function bike(){
+    document.getElementById("car").src="https://www.freepnglogos.com/uploads/bike-png/black-yamaha-yzf-sport-motorcycle-bike-png-image-pngpix-32.png"
+}
+function cycle(){
+    document.getElementById("car").src="https://i.pinimg.com/originals/fc/0f/4c/fc0f4cc14a660cabddd7a92ddc56f30c.png"
+}
